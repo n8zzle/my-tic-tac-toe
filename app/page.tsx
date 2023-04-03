3e9a0bb204d796bdeb5e1c73d8eb5f723a17ce2d
n8zzle <playwithn8zzle@gmail.com>
2023-04-03 09:30:57 +0200

    fixing bugs for deploymentw

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { motion } from "framer-motion";
 
